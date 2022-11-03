51e8a5d0074a253573d5a58654d3e7f20c46c99f
sky92 <sky92@DESKTOP-P8SB3R8>
2022-11-03 18:59:14 +0800

    202211031859

--- ImGuiDemoGlewLearn/src/Widget.cpp
+++ ImGuiDemoGlewLearn/src/Widget.cpp
@@ -11,8 +11,7 @@
 /// <param name="window"></param>
 /// <returns></returns>
 GLFWwindow* Widget::CreateWindows(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* window)
-{
-
+{ 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -30,8 +29,6 @@ GLFWwindow* Widget::CreateWindows(int width, int height, const char* title, GLFW
 	glfwMakeContextCurrent(HWindow);
 	return HWindow;
 }
-
-
 /// <summary>
 /// 设定窗体
 /// </summary>
@@ -52,6 +49,74 @@ void Widget::WindowSetting(int width, int height)
 	return;
 }
 
+/// <summary>
+/// 初始化imgui
+/// </summary>
+/// <param name="HWindows"></param>
+void Widget::InitImGui(GLFWwindow* HWindows)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext(NULL);
+
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+
+	//风格
+	ImGui::StyleColorsDark();
+	io.ConfigViewportsNoAutoMerge=true;
+
+	//io.Fonts->AddFontFromFileTTF("R:/Z_BACKUP_ORDER/_PROJECT_/CPP/ImGuiDemo/_dependency_/Fonts/simhei.ttf",15.0f,NULL,io.Fonts->GetGlyphRangesChineseFull());
+	//找到当前目录并定位字体文件
+	std::string fontPathStr = GetCWD() + "/Fonts/simhei.ttf";  
+	std::cout << fontPathStr<<std::endl;
+	const char* fontPath = fontPathStr.c_str(); 
+	io.Fonts->AddFontFromFileTTF(fontPath,15.0f,NULL,io.Fonts->GetGlyphRangesChineseFull());
+	//io.Fonts->AddFontFromMemoryCompressedTTF
+	ImGui_ImplGlfw_InitForOpenGL(HWindows, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	//docking setting
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiViewportFlags_NoDecoration;  
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	//io.ConfigFlags |= ImGuiCol_DockingEmptyBg;
+
+}
+
+/// <summary>
+/// 运行imgui
+/// </summary>
+void Widget::RunWidget()
+{
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+	//设定在主窗体上可停靠子窗体
+	ImGui::DockSpaceOverViewport();
+
+	//官方实例
+	ImGui::ShowDemoWindow();
+
+	//由bool控制的imgui子窗
+	if (editor_Bool)
+	{
+		WidgetTest();
+	}
+	//运行 imgui菜单栏子窗
+	MenuBarTest();
+
+	//开始整体渲染
+	WidgetRender();
+
+}
+
+void Widget::CloseWidget()
+{
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+}
+  
 
 
 bool editor_Bool;
@@ -94,70 +159,3 @@ void WidgetRender() {
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-/// <summary>
-/// 运行imgui
-/// </summary>
-void Widget::RunWidget()
-{
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
-	//设定在主窗体上可停靠子窗体
-	ImGui::DockSpaceOverViewport();
-
-	//官方实例
-	ImGui::ShowDemoWindow();
-	  
-	//由bool控制的imgui子窗
-	if (editor_Bool)
-	{
-		WidgetTest();
-	}
-	//运行 imgui菜单栏子窗
-	MenuBarTest();
-	 
-	//开始整体渲染
-	WidgetRender();
-
-}
-
-void Widget::CloseWidget()
-{
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-}
-
-/// <summary>
-/// 初始化imgui
-/// </summary>
-/// <param name="HWindows"></param>
-void Widget::InitImGui(GLFWwindow* HWindows)
-{
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext(NULL);
-
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
-
-	//风格
-	ImGui::StyleColorsDark();
-	io.ConfigViewportsNoAutoMerge=true;
-
-	//io.Fonts->AddFontFromFileTTF("R:/Z_BACKUP_ORDER/_PROJECT_/CPP/ImGuiDemo/_dependency_/Fonts/simhei.ttf",15.0f,NULL,io.Fonts->GetGlyphRangesChineseFull());
-	//找到当前目录并定位字体文件
-	std::string fontPathStr = GetCWD() + "/Fonts/simhei.ttf";  
-	std::cout << fontPathStr<<std::endl;
-	const char* fontPath = fontPathStr.c_str(); 
-	io.Fonts->AddFontFromFileTTF(fontPath,15.0f,NULL,io.Fonts->GetGlyphRangesChineseFull());
-	//io.Fonts->AddFontFromMemoryCompressedTTF
-	ImGui_ImplGlfw_InitForOpenGL(HWindows, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-
-	//docking setting
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-	io.ConfigFlags |= ImGuiViewportFlags_NoDecoration;  
-	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-	//io.ConfigFlags |= ImGuiCol_DockingEmptyBg;
-
-}
--- ImGuiDemoGlewLearn/src/main.cpp
+++ ImGuiDemoGlewLearn/src/main.cpp
@@ -2,73 +2,40 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
-
-
-//#include "imgui.h"
-//#include "imgui_impl_opengl3.h"
-//#include "imgui_impl_glfw.h"
+  
+#include "Widget.h"  
 
 #define SCR_WIDTH 1200 
 #define SCR_HEIGHT 800 
-
-//#define GLEW_STATIC
-//#include  <GL/glew.h>
-//#define GLFW_STATIC
-//#include  <GLFW/glfw3.h>
-
-#include "Widget.h" 
-
- 
-
 int main() {
 
 	printf("start\n");  
 
-
+	//窗口功能实例化
 	Widget windowFunc =  Widget::Widget();
+	//创建窗口
 	GLFWwindow* HWindow = windowFunc.CreateWindows(SCR_WIDTH, SCR_HEIGHT,"title",NULL, NULL); 
-	/*glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
-	GLFWwindow* HWindow = glfwCreateWindow(800, 600, "title", NULL, NULL); 
-	if (HWindow == NULL)
-	{
-		printf("create window failed\n");
-		glfwTerminate();
-		return -1;
-	} 
-	glfwMakeContextCurrent(HWindow);*/
-
+	//设定窗口
 	windowFunc.WindowSetting(SCR_WIDTH, SCR_HEIGHT);
-	/*glewExperimental = true;
-	if (glewInit() != GLEW_OK)
-	{
-		printf("glew failed\n");
-		glfwTerminate();
-		return -1;
-	} 
-	glViewport(0, 0, 800, 600);
-	glEnable(GL_DEPTH_TEST);*/
-
+	//初始化imgui
 	windowFunc.InitImGui(HWindow);
+
 	//界面循环
 	while (!glfwWindowShouldClose(HWindow))
 	{
+		//每帧清屏
 		glClearColor(0.f, 0.5f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+		//绘制窗口
 		windowFunc.RunWidget();
-
+		//每帧清缓存
 		glfwSwapBuffers(HWindow);
 		glfwPollEvents();
 	}
+
+	//关闭窗口后的处理   imgui的内存释放等
+	windowFunc.CloseWidget();
 	 
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-
 	return 0;
 }
 
